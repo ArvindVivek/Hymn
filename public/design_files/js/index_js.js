a3c81738a654bf439fd5af6bc01f772efd7a4153
Justin Yue <flyingjay16@gmail.com>
2020-03-28 00:51:57 -0700

    authentication v1

---
+++ public/design_files/js/index_js.js
@@ -0,0 +1,42 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBkGmqg0xyAPT7yuru-SdBpMXucpeUIwEI",
+    authDomain: "lahacks2020-aedfb.firebaseapp.com",
+    databaseURL: "https://lahacks2020-aedfb.firebaseio.com",
+    projectId: "lahacks2020-aedfb",
+    storageBucket: "lahacks2020-aedfb.appspot.com",
+    messagingSenderId: "817369168992",
+    appId: "1:817369168992:web:804654ce3e747cdf7ed705",
+    measurementId: "G-V62EZTESV5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const auth = firebase.auth();
+
+function signup() {
+    window.location = "./signup.html";
+}
+
+function login() {
+    var user = document.getElementById('user_field').value;
+    var password = document.getElementById('password_field').value;
+
+    if (user == "" || password == "") {
+        window.alert("The username and/or password field(s) are empty. Please fill them out.");
+    }
+    else {
+        const promise = auth.signInWithEmailAndPassword(user, password);
+        promise.catch(e => alert(e.message));
+    }
+}
+
+
+auth.onAuthStateChanged(firebaseUser => {
+    if (firebaseUser) {
+
+    }
+    else {
+        //window.location = "./index.html";
+    }
+});
---
+++ public/design_files/js/signup_js.js
@@ -0,0 +1,37 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBkGmqg0xyAPT7yuru-SdBpMXucpeUIwEI",
+    authDomain: "lahacks2020-aedfb.firebaseapp.com",
+    databaseURL: "https://lahacks2020-aedfb.firebaseio.com",
+    projectId: "lahacks2020-aedfb",
+    storageBucket: "lahacks2020-aedfb.appspot.com",
+    messagingSenderId: "817369168992",
+    appId: "1:817369168992:web:804654ce3e747cdf7ed705",
+    measurementId: "G-V62EZTESV5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  const auth = firebase.auth();
+
+  function signup() {
+      var user = document.getElementById("user_field").value;
+      var password = document.getElementById("password_field").value;
+
+      if(user == "" || password == "") {
+          window.alert("The username and/or password field(s) are empty. Please fill them out.");
+      }
+      else {
+          const promise = auth.createUserWithEmailAndPassword(user, password);
+          promise.catch(e => alert(e.message));
+      }
+  }
+
+  auth.onAuthStateChanged(firebaseUser => {
+    if(firebaseUser) {
+        window.location = "./home.html";
+    }
+    else {
+
+    }
+  });
