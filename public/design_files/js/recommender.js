3e247b7931d87268a230718be321836ce1bc06d9
rice31415 <ericwang3.1415@gmail.com>
2020-03-28 21:07:14 -0700

    Update recommender.js

--- public/design_files/js/recommender.js
+++ public/design_files/js/recommender.js
@@ -1,39 +1,135 @@
-var intensity = 1;
-var muscles = [ ["Chest", 0, 1, recChest()], 
-                ["Shoulder", 0, 1, recShoulder()], 
-                ["Back", 1, 1, recBack()], 
+var intensity = 3;
+var lvl =   [ ["Dive Bomber Pushups", "Decline Pushups"], 
+            ["Bodyweight Side Lateral Raises", "Handstand Push-Ups"], 
+            ["Inverted Row","Pull-Ups"], 
+            ["Cobra Push-Ups", "Diamond Push-Ups"],
+            ["Chin-Ups","Pull-Ups"],
+            ["Sprinter Lunges", "Bulgarian Split Squats"],
+            ["Single Leg Glute Bridges", "Slick Floor Bridge Curls"],
+            ["Bodyweight Calf Raises", "Jump Squats"],
+            ["Mountain Climbers", "Russian Twists"]
+            ];
+var muscles = [ ["Chest", 2, 1, recChest()], 
+                ["Shoulder", 4, 4, recShoulder()], 
+                ["Back", 2, 1, recBack()], 
                 ["Triceps", 0, 1, recTriceps()],
                 ["Biceps", 2, 1, recBiceps()],
                 ["Quadriceps", 2, 1, recQuadriceps()],
                 ["Hamstrings", 2, 1, recHamstrings()],
-                ["Calf", 2, 1, recCalf()]
+                ["Calf", 2, 1, recCalf()],
+                ["Cardio", 2, 1, recCardio()]
                 ];
                 //name, days done, intensity level
+function applyPreferences() {
+    for (var i = 0; i < muscles.length; i++) {
+        muscles[i][1] /= muscles[i][2];
+        Math.floor(muscles[i][1]);
+    }
+}
 
 function identifyAreas() {
-    var copy = muscles;
     var least = 0;
     var arr = [];
     var save = [];
     for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < copy.length; j++) {
-            if (copy[least][1] > copy[j][1]) {
+        for (var j = 0; j < muscles.length; j++) {
+            if (muscles[least][1] > muscles[j][1]) {
                 least = j;
             }
         }
         arr[i] = least;
-        save[i] = copy[least][1];
-        copy[least][1] = 14;
+        save[i] = muscles[least][1];
+        muscles[least][1] = 14;
     }
     for (var i = 0; i < arr.length; i++) {
-        copy[arr[i]][1] = save[i]
+        muscles[arr[i]][1] = save[i]
     }
     return arr;
 }
 
 
+function recChest() {
+    var ex = "Pushups - # of Sets: " + String(Math.round(2+intensity)) + " # of Reps: " + String(10) + "<br>";
+    if (intensity > 1) {
+        ex += "For your intensity level, we'd also like to suggest trying " + lvl[0][intensity-2] + "<br>";
+    }
+    return ex.link("https://www.google.com");
+}
+
+function recShoulder() {
+    var ex = "Plank Tuck Jumps - # of Sets: " + String(Math.round(2+intensity)) + " # of Reps: " + String(10) + "<br>";
+    if (intensity > 1) {
+        ex += "For your intensity level, we'd also like to suggest trying " + lvl[1][intensity-2] + "<br>";
+    }
+    return ex;
+}
+
+function recBack() {
+    var ex = "Superman - # of Sets: " + String(Math.round(2+intensity)) + " # of Seconds: " + String(30) + "<br>";
+    if (intensity > 1) {
+        ex += "For your intensity level, we'd also like to suggest trying " + lvl[2][intensity-2] + "<br>";
+    }
+    return ex;
+}
+
+function recTriceps() {
+    var ex = "Knee Cobra Pushups - # of Sets: " + String(Math.round(2+intensity)) + " # of Reps: " + String(10) + "<br>";
+    if (intensity > 1) {
+       ex += "For your intensity level, we'd also like to suggest trying " + lvl[3][intensity-2] + "<br>";
+    }
+    return ex;
+}
+
+function recBiceps() {
+    var ex = "Curls - # of Sets: " + String(Math.round(2+intensity)) + " # of Reps: " + String(10) + "<br>";
+    if (intensity > 1) {
+       ex += "For your intensity level, we'd also like to suggest trying " + lvl[4][intensity-2] + "<br>";
+    }
+    return ex;
+}
+
+function recQuadriceps() {
+    var ex = "Lunges - # of Sets: " + String(Math.round(2+intensity)) + " # of Reps: " + String(10) + "<br>";
+    if (intensity > 1) {
+       ex += "For your intensity level, we'd also like to suggest trying " + lvl[5][intensity-2] + "<br>";
+    }
+    return ex;
+}
+
+function recHamstrings() {
+    var ex = "Hip Raises - # of Sets: " + String(Math.round(2+intensity)) + " # of Reps: " + String(10) + "<br>";
+    if (intensity > 1) {
+       ex += "For your intensity level, we'd also like to suggest trying " + lvl[6][intensity-2] + "<br>";
+    }
+    return ex;
+}
+
+function recCalf() {
+    var ex = "High Knees - # of Sets: " + String(Math.round(2+intensity)) + " # of Seconds: " + String(30) + "<br>";
+    if (intensity > 1) {
+       ex += "For your intensity level, we'd also like to suggest trying " + lvl[7][intensity-2] + "<br>";
+    }
+    return ex;
+}
+
+function recCardio() {
+    var ex = "Jumping Jacks - # of Sets: " + String(Math.round(2+intensity)) + " # of Reps: " + String(15) + "<br>"; 
+    if (intensity > 1) {
+       ex += "For your intensity level, we'd also like to suggest trying " + lvl[8][intensity-2] + "<br>";
+    }
+    return ex;
+}
+
+function needRest() {
+    var sum = 0;
+    for (var i = 0; i < muscles.length; i++) {
+        sum += muscles[i][1];
+    }
+    return ((sum/3) > 7-Math.round(2/intensity));
+}
 
 function main() {
+    applyPreferences();
     var msg = "";
     var arr = identifyAreas();
     if (needRest()) {
@@ -49,56 +145,4 @@ function main() {
         }
     }
     document.getElementById("test").innerHTML = msg;
-
-    //else if (identifyAreas() == muscles[3][0]) {
-        //document.getElementById("test").innerHTML = recCardio();
-    //}
-}
-
-function recChest() {
-    var ex = "Insert Chest Exercise: " + "# of Sets: " + String(Math.round(2*intensity)) + " # of Reps: " + String(10) + "<br>";
-    return ex.link("https://www.google.com");
-}
-
-function recShoulder() {
-    var ex = "Insert Shoulder Exercise: " + "# of Sets: " + String(Math.round(2*intensity)) + " # of Reps: " + String(10) + "<br>";
-    return ex;
-}
-
-function recBack() {
-    var ex = "Insert Back Exercise: " + "# of Sets: " + String(Math.round(2*intensity)) + " # of Reps: " + String(10) + "<br>";
-    return ex;
-}
-
-function recTriceps() {
-    var ex = "Insert Triceps Exercise: " + "# of Sets: " + String(Math.round(2*intensity)) + " # of Reps: " + String(10) + "<br>";
-    return ex;
-}
-
-function recBiceps() {
-    var ex = "Insert Biceps Exercise: " + "# of Sets: " + String(Math.round(2*intensity)) + " # of Reps: " + String(10) + "<br>";
-    return ex;
-}
-
-function recQuadriceps() {
-    var ex = "Insert Quadriceps Exercise: " + "# of Sets: " + String(Math.round(2*intensity)) + " # of Reps: " + String(10) + "<br>";
-    return ex;
-}
-
-function recHamstrings() {
-    var ex = "Insert Hamstrings Exercise: " + "# of Sets: " + String(Math.round(2*intensity)) + " # of Reps: " + String(10) + "<br>";
-    return ex;
-}
-
-function recCalf() {
-    var ex = "Insert Calf Exercise: " + "# of Sets: " + String(Math.round(2*intensity)) + " # of Reps: " + String(10) + "<br>";
-    return ex;
-}
-
-function needRest() {
-    var sum = 0;
-    for (var i = 0; i < muscles.length; i++) {
-        sum += muscles[i][1];
-    }
-    return ((sum/3) > 14-(2/intensity));
 }
